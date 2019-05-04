7a79bea20413f992400b2eedb7488c2886334b59
Erwin Eu <eu.erwin@xie-group.com>
2019-05-04 13:41:09 +0200

    First commit

---
+++ LibraryNameBundle.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * This file is part of the Wine application.
+ *
+ * (c) Wine <http://www.coruja.studio/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.#
+ *
+ * @author Erwin Eu <eu.erwin@coruja.studio>
+ * @developer Erwin Eu<eu.erwin@coruja.studio>
+ */
+namespace Prefix\Bundle\LibraryNameBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class LibraryNameBundle
+ *
+ * @author Erwin Eu <eu.erwin@coruja.studio>
+ */
+class LibraryNameBundle extends Bundle
+{
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+README
+==============
+
+to use this as template for developing third party library:
+
+clone this repository on your lib/YourLibraryName
+
+and then add this line on your main composer autoload
+
+```
+    ...
+    "YourLibraryNameSpace\\": "lib/YourLibraryName/"
+    ...
+```
+
+Resources
+---------
